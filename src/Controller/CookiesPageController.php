0c4783838f826c6445cb8a95bdbee9a61c30ba4b
Tom <tom@kees-tm.nl>
2018-09-19 14:24:26 +0200

    Format code

--- src/Controller/CookiesPageController.php
+++ src/Controller/CookiesPageController.php
@@ -3,65 +3,66 @@ namespace Drupal\kees_cookiebar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-
-class CookiesPageController extends ControllerBase {
+class CookiesPageController extends ControllerBase
+{
 
   /**
    * Display the markup.
    *
    * @return array
    */
-  public function content() {
+    public function content()
+    {
     // Read settings
-    $config = \Drupal::config('kees_cookiebar.settings');
+        $config = \Drupal::config('kees_cookiebar.settings');
 
-    return array(
-        '#theme' => 'kees_cookiesPage',
-        '#title' => array(
-            '#markup' =>$config->get('kees_cookiebar.page_title'),
-        ),
-        '#intro' => array(
-            '#markup' =>$config->get('kees_cookiebar.page_intro'),
-        ),
-        '#text' => array(
-            '#markup' =>$config->get('kees_cookiebar.page_text'),
-        ),
-        '#accept_button_text' => array(
-            '#markup' =>$config->get('kees_cookiebar.page_accept_button_text'),
-        ),
-        '#decline_button_text' => array(
-            '#markup' =>$config->get('kees_cookiebar.page_decline_button_text'),
-        ),
-        '#cookieValue' => array(
-            '#markup' => $this->getCookieValue(),
-        ),
-        '#attached' => array(
-            'library' => array(
-              'kees_cookiebar/cookiebar-css',
-              'kees_cookiebar/cookiebar-js',
+        return array(
+            '#theme' => 'kees_cookiesPage',
+            '#title' => array(
+                '#markup' =>$config->get('kees_cookiebar.page_title'),
             ),
-        ),
-        '#cache' => array(
-            "max-age" => 0,
-            "contexts" => array(
-                "cookies:CookieConsent",
+            '#intro' => array(
+                '#markup' =>$config->get('kees_cookiebar.page_intro'),
             ),
-        ),        
-    );
-  }
-  
+            '#text' => array(
+                '#markup' =>$config->get('kees_cookiebar.page_text'),
+            ),
+            '#accept_button_text' => array(
+                '#markup' =>$config->get('kees_cookiebar.page_accept_button_text'),
+            ),
+            '#decline_button_text' => array(
+                '#markup' =>$config->get('kees_cookiebar.page_decline_button_text'),
+            ),
+            '#cookieValue' => array(
+                '#markup' => $this->getCookieValue(),
+            ),
+            '#attached' => array(
+                'library' => array(
+                'kees_cookiebar/cookiebar-css',
+                'kees_cookiebar/cookiebar-js',
+                ),
+            ),
+            '#cache' => array(
+                "max-age" => 0,
+                "contexts" => array(
+                    "cookies:CookieConsent",
+                ),
+            ),
+        );
+    }
+
 /**
  * Read the value of the cookie used to track the users preference
  *
  * @return bool
- */    
-private function getCookieValue(){
-    $cookie_name = "CookieConsent";
+ */
+    private function getCookieValue()
+    {
+        $cookie_name = "CookieConsent";
 
-    if(isset($_COOKIE[$cookie_name])) {
-        return $_COOKIE[$cookie_name];
-    } 
-    return false;
-}
-
+        if (isset($_COOKIE[$cookie_name])) {
+            return $_COOKIE[$cookie_name];
+        }
+        return false;
+    }
 }
