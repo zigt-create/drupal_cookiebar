61db752751408fd4cc01777a7545ac72c5be6bf6
Tom <tom@kees-tm.nl>
2018-09-20 16:00:04 +0200

    Create new JS to ser cookie

--- assets/js/kees-cookiebar.js
+++ assets/js/kees-cookiebar.js
@@ -1,70 +1,25 @@
-(function ($) {
+function keesCookieForm() {
 
-  // When clicking on the OK button in the cookiebar
-  $(document).on('click', '.kees-js-cookiebar-container a.kees-js-cookiebar-button', function(event) {
-    // Variables
-    var $object = $(this);
+    var cookieConsent = {};
 
-    // Prevent following the href
-    event.preventDefault();
+    $("form#kees_cookie_form input:checkbox").each(function (index) {
+        var value = $(this).is(":checked");
+        var id = $(this).prop('name');
 
-    // SetCookie function
-    setCookie( ($object.attr("id") == "true")? "true": "false" );
+        cookieConsent[id] = value;
 
-    // Reload page to apply needed and remove unwanted cookies
-    location.reload();
-  });
+    });
+    cookieConsent.primary_cookies = true; // set default for primary cookies
 
-  // When clicking on the radio buttons on the cookie page
-  $(document).on('click', '#kees-js-cookiebar-settings input[type=radio]', function(event) {
-    // Variables
-    var $object = $(this);
-
-    // SetCookie function
-    setCookie( ($object.val() == "true")? "true": "false" );
-
-    // Reload page to apply needed and remove unwanted cookies
-    location.reload();
-  });
-
-
-  function setCookie(value){
-
-    // Set cookiebar cookie for one year to the value which can be 'CookieAllowed' or 'CookieDisallowed'
     var oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
-    document.cookie = "CookieConsent="+ value +';expires=' + oneYearFromNow.toGMTString() + '; path=/';
+    document.cookie = "CookieConsent=" + JSON.stringify(cookieConsent) + ';expires=' + oneYearFromNow.toGMTString() + '; path=/';
 
-    // Additional remove all other cookies if value is 'CookieDisallowed'
-    if("false" == value) {
-      // Get all cookies in array
-      var getCookies = document.cookie.split(';');
+    return true;
+}
 
-      // Loop through all cookies
-      $.each(getCookies, function(index, value){
-
-        // Remove whitespaces from value
-        value = value.replace(/^\s+|\s+$/g, '');
-
-        // Split value on equal sign to extract name and cookievalue
-        var splitValue = value.split('=');
-
-        // Set variables with the name and value and time in past to remove the cookie
-        var cookieRemoveExpireTime = new Date();
-        cookieRemoveExpireTime.setTime(cookieRemoveExpireTime.getTime() - 1);
-
-        var cookieName = splitValue[0];
-
-        // If cookiename is not our own cookiebar cookie
-        if('CookieConsent' != cookieName) {
-          document.cookie = cookieName + "=; expires=" + cookieRemoveExpireTime.toGMTString() + "; path=/";
-        }
-
-      });
-    }
-  }
-
-  $(document).ready(function(){
-        $('.consent__form__trigger').click(function(){
+(function ($) {
+    $(document).ready(function () {
+        $('.consent__form__trigger').click(function () {
             $(this).toggleClass('minus');
             $(this).next().toggleClass('open');
         });
