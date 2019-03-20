5334f3a894bb66b638be449088a355e7150fcd8b
Tom Grootjans <tom@kees-tm.nl>
2019-03-20 15:58:02 +0100

    Update JS for advanced cookiebar to work with Drupal.behaviors

--- assets/js/cookiebar-advanced.js
+++ assets/js/cookiebar-advanced.js
@@ -6,72 +6,76 @@
  */
 
 /* Set default variables */
-var cookieName = 'CookieConsent';
+var keesCookieName = 'CookieConsent';
 
-/**
- * Jquery on document ready function(s)
- */
-jQuery(document).ready(function () {
+(function ($, Drupal) {
+    Drupal.behaviors.kees_cookiebar_advanced = {
+        attach: function (context, drupalSettings) {
+            var cookiepagePath = drupalSettings.keesCookiebarConfig.cookiepagePath;
+            var currentPath = window.location.pathname;
 
-    // Show cookies block
-    var cookiesPage = jQuery('#cookiebar_container').data('cookies-url');
-    if (getCookieValue('primary_cookies') != true || cookiesPage == window.location.pathname) {
-        jQuery('#cookiebar_container').show();
+            if (getCookieValue('primary_cookies') != true || currentPath == cookiepagePath) {
+                $('#kees-cookiebar-container', context).show();
+            }
+
+            $('.consent__form__trigger', context).click(function () {
+                $(this).toggleClass('minus');
+                $(this).next().toggleClass('open');
+            });
+
+            // On form submit, save selected value as cookie
+            $('form#kees-cookiebar-form', context).on('submit', function (e) {
+                e.preventDefault();  //prevent form from submitting
+
+                var cookie = {};
+
+                $("form#kees-cookiebar-form input:checkbox").each(function () {
+                    var value = $(this).is(":checked");
+                    var id = $(this).prop('name');
+
+                    cookie[id] = value;
+
+                });
+                cookie.primary_cookies = true; // set default for primary cookies
+
+                var oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+                document.cookie = "CookieConsent=" + JSON.stringify(cookie) + ';expires=' + oneYearFromNow.toGMTString() + '; path=/';
+
+                // Redirect or reload the page
+                if (currentPath == cookiepagePath) {
+                    window.location.href = "/";
+                } else {
+                    location.reload();
+                }
+            });
+        }
+    };
+
+    /**
+     * Get a settings from the cookie
+     *
+     * @param {string} name the name of the setting to get from the cookie defined in this file
+     */
+    function getCookieValue(name) {
+        var cookie = getCookie();
+
+        try {
+            var object = JSON.parse(cookie);
+        } catch (e) {
+            return false;
+        }
+
+        return object[name];
     }
 
-    jQuery('.consent__form__trigger').click(function () {
-        jQuery(this).toggleClass('minus');
-        jQuery(this).next().toggleClass('open');
-    });
-});
+    /**
+     * Get the cookie set by this module
+     */
+    function getCookie() {
+        var value = "; " + document.cookie;
+        var cookie = value.split("; " + keesCookieName + "=");
 
-/**
- * Functions that sets a cookiebased on cookieform
- *      Function is called bij the sibmit button on the cookieform
- *
- * @returns {boolean} always true to continue the form sumbit
- */
-function keesCookieForm() {
-    var cookie = {};
-
-    jQuery("form#kees_cookie_form input:checkbox").each(function (index) {
-        var value = jQuery(this).is(":checked");
-        var id = jQuery(this).prop('name');
-
-        cookie[id] = value;
-
-    });
-    cookie.primary_cookies = true; // set default for primary cookies
-
-    var oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
-    document.cookie = "CookieConsent=" + JSON.stringify(cookie) + ';expires=' + oneYearFromNow.toGMTString() + '; path=/';
-
-    return true;
-}
-
-/**
- * Function to get a settings from the cookie
- *
- * @param {string} name the name of the setting to get from the cookie defined in this file
- */
-function getCookieValue(name) {
-    var cookie = getCookie();
-
-    try {
-        var object = JSON.parse(cookie);
-    } catch (error) {
-        return false;
+        if (cookie.length == 2) return cookie.pop().split(";").shift();
     }
 
-    return object[name];
-}
-
-/**
- * Get the cookie set by this module
- */
-function getCookie() {
-    var value = "; " + document.cookie;
-    var cookie = value.split("; " + cookieName + "=");
-
-    if (cookie.length == 2) return cookie.pop().split(";").shift();
-}
+})(jQuery, Drupal);
